dcb15b295b78ce40b044e7afd71ec8e23ffeb6cb
Felix <felix.rocke@gmx.de>
2023-12-18 12:56:26 +0000

    updated benchmarking

--- experiments/bert/bert.py
+++ experiments/bert/bert.py
@@ -1,3 +1,4 @@
+import tensorflow as tf
 from timeit import Timer
 from transformers import DistilBertTokenizer, TFDistilBertModel, BertTokenizer, TFBertModel
 
@@ -5,8 +6,8 @@ EXPERIMENT_NAME = "bert/"
 RESULTS_PATH = "results/"
 FILE_NAME_COMPRESSED_RESULTS = "results-compressed.txt"
 
-def benchCompressed(tokenizer, model, reps, iters, modelName):
-    fileName = RESULTS_PATH + EXPERIMENT_NAME + modelName + ".txt"
+def benchUncompiled(tokenizer, model, reps, iters, modelName):
+    fileName = RESULTS_PATH + EXPERIMENT_NAME + modelName + "-uncompiled.txt"
     f = open(fileName, "w")
     results = ""
     for i in range(reps):
@@ -19,14 +20,15 @@ def benchCompressed(tokenizer, model, reps, iters, modelName):
     f.write(results)
     f.close()
 
-def benchUncompressed(tokenizer, model, reps, iters, modelName):
-    fileName = RESULTS_PATH + EXPERIMENT_NAME + modelName + ".txt"
+def benchCompiled(tokenizer, model, reps, iters, modelName):
+    fileName = RESULTS_PATH + EXPERIMENT_NAME + modelName + "-compiled.txt"
     f = open(fileName, "w")
     results = ""
+    xla_model = tf.function(model, jit_compile=True)
     for i in range(reps):
         for input_string in ["TensorFlow is", "TensorFlow is a", "TFLite is a"]:
             tokenized_input = tokenizer(input_string, return_tensors='tf')
-            t = Timer(lambda: model(tokenized_input))
+            t = Timer(lambda: xla_model(tokenized_input))
             print(t.timeit(number=iters)/iters)
             results += str(t.timeit(number=iters)/iters) + "\n"
 
@@ -40,11 +42,15 @@ def main():
         if modelName == "distilbert-base-uncased":
             tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
             model = TFDistilBertModel.from_pretrained("distilbert-base-uncased")
-            benchCompressed(tokenizer=tokenizer, model=model, reps=1, iters=100, modelName=modelName)
+            benchUncompiled(tokenizer=tokenizer, model=model, reps=1, iters=1, modelName=modelName)
+            benchCompiled(tokenizer=tokenizer, model=model, reps=1, iters=1, modelName=modelName)
+
         else: 
             tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
             model = TFBertModel.from_pretrained("bert-base-uncased")
-            benchUncompressed(tokenizer=tokenizer, model=model, reps=1, iters=100, modelName=modelName)
+            benchUncompiled(tokenizer=tokenizer, model=model, reps=1, iters=1, modelName=modelName)
+            benchCompiled(tokenizer=tokenizer, model=model, reps=1, iters=1, modelName=modelName)
+
 
 
 if __name__ == "__main__":

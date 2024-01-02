7cbcd8c92dd74a9a5e82c2b3b99f9a864e12efb5
Felix <felix.rocke@gmx.de>
2024-01-02 14:31:58 +0100

    renamed pruning file

---
+++ scripts/pruning/magnitude_based_pruning.py
@@ -0,0 +1,129 @@
+# BASED ON https://www.tensorflow.org/model_optimization/guide/pruning/pruning_with_keras
+import tempfile
+import tensorflow as tf
+import numpy as np
+import os
+import zipfile
+
+from tensorflow import keras
+import tensorflow_model_optimization as tfmot
+
+
+def get_lenet_model():
+    # LeNet-5 model
+    return tf.keras.models.Sequential([
+        tf.keras.layers.Conv2D(6, kernel_size=(5, 5), activation='relu', input_shape=(28, 28, 1)),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.Conv2D(16, kernel_size=(5, 5), activation='relu'),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(120, activation='relu'),
+        tf.keras.layers.Dense(84, activation='relu'),
+        tf.keras.layers.Dense(10, activation='softmax')
+    ])
+
+
+def get_gzipped_model_size(file):
+    # Returns size of gzipped model, in bytes.
+    _, zipped_file = tempfile.mkstemp('.zip')
+    with zipfile.ZipFile(zipped_file, 'w', compression=zipfile.ZIP_DEFLATED) as f:
+        f.write(file)
+
+    return os.path.getsize(zipped_file)
+
+
+def main():
+    mnist = keras.datasets.mnist
+    (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+    # Normalize the input image so that each pixel value is between 0 and 1.
+    train_images = train_images / 255.0
+    test_images = test_images / 255.0
+
+    model = get_lenet_model()
+
+    # Train the digit classification model
+    model.compile(optimizer='adam',
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
+
+    model.fit(
+      train_images,
+      train_labels,
+      epochs=6,
+      validation_split=0.1,
+    )
+
+    _, baseline_model_accuracy = model.evaluate(test_images, test_labels, verbose=0)
+
+    print('Baseline test accuracy:', baseline_model_accuracy)
+
+    _, keras_file = tempfile.mkstemp('.h5')
+    tf.keras.models.save_model(model, keras_file, include_optimizer=False)
+    print('Saved baseline model to:', keras_file)
+
+    prune_low_magnitude = tfmot.sparsity.keras.prune_low_magnitude
+
+    # Compute end step to finish pruning after 2 epochs.
+    batch_size = 128
+    epochs = 2
+    validation_split = 0.1  # 10% of training set will be used for validation set.
+
+    num_images = train_images.shape[0] * (1 - validation_split)
+    end_step = np.ceil(num_images / batch_size).astype(np.int32) * epochs
+
+    # Define model for pruning.
+    pruning_params = {
+          'pruning_schedule': tfmot.sparsity.keras.PolynomialDecay(initial_sparsity=0.50,
+                                                                   final_sparsity=0.80,
+                                                                   begin_step=0,
+                                                                   end_step=end_step),
+    }
+
+    model_for_pruning = prune_low_magnitude(model, **pruning_params)
+
+    # `prune_low_magnitude` requires a recompile.
+    model_for_pruning.compile(optimizer='adam',
+                              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                              metrics=['accuracy'])
+
+    model_for_pruning.summary()
+
+    logdir = tempfile.mkdtemp()
+
+    callbacks = [
+      tfmot.sparsity.keras.UpdatePruningStep(),
+      tfmot.sparsity.keras.PruningSummaries(log_dir=logdir),
+    ]
+
+    model_for_pruning.fit(train_images, train_labels,
+                          batch_size=batch_size,
+                          epochs=epochs,
+                          validation_split=validation_split,
+                          callbacks=callbacks)
+
+    _, model_for_pruning_accuracy = model_for_pruning.evaluate(
+       test_images, test_labels, verbose=0)
+
+    print('Baseline test accuracy:', baseline_model_accuracy)
+    print('Pruned test accuracy:', model_for_pruning_accuracy)
+
+    model_for_export = tfmot.sparsity.keras.strip_pruning(model_for_pruning)
+
+    _, pruned_keras_file = tempfile.mkstemp('.h5')
+    tf.keras.models.save_model(model_for_export, pruned_keras_file, include_optimizer=False)
+    print('Saved pruned Keras model to:', pruned_keras_file)
+
+    print("Size of gzipped baseline Keras model: %.2f bytes" % (get_gzipped_model_size(keras_file)))
+    print("Size of gzipped pruned Keras model: %.2f bytes" % (get_gzipped_model_size(pruned_keras_file)))
+
+    model_for_export.compile(optimizer='adam',
+                             loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                             metrics=['accuracy'])
+
+    model.evaluate(test_images, test_labels)
+    model_for_export.evaluate(test_images, test_labels)
+
+
+if __name__ == "__main__":
+    main()

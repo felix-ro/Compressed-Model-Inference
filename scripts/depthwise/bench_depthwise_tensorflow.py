40337a61bffc635ce81fb2f51f19e6aa2b684a53
Felix <felix.rocke@gmx.de>
2024-01-11 14:26:41 +0000

    updated depthwise measurements

--- scripts/depthwise/bench_depthwise_tensorflow.py
+++ scripts/depthwise/bench_depthwise_tensorflow.py
@@ -1,57 +1,77 @@
 import tensorflow as tf
 import timeit
 
-inp = tf.random.normal((2, 64, 64, 3))
+input = tf.random.normal((2, 64, 64, 3))
+kernel_size = 1
 
 
 def conv2d():
-    tf.keras.layers.Conv2D(16, 3, strides=1, padding='same')(inp)
+    tf.keras.layers.Conv2D(filters=16, kernel_size=kernel_size, strides=1, padding='same')(input)
 
 
-def conv2dDepthwise():
-    tf.keras.layers.DepthwiseConv2D(3, strides=1, padding='same')(inp)
+def conv2dSeparable():
+    tf.keras.layers.SeparableConv2D(filters=16, kernel_size=kernel_size, strides=1, padding='same')(input)
 
 
-def benchmark(input):
-    global inp
-    inp = input
-    print("Input shape: " + str(inp.shape))
-
-    # warum up
-    conv2d()
-    conv2dDepthwise()
+def benchmark(inp, kern_size):
+    global input, kernel_size
+    input = inp
+    kernel_size = kern_size
+    print(f"Input shape: {input.shape}")
+    print(f"Kernel size: {kernel_size}")
 
     # measure
     num_iterations = 100
+
+    conv2d()  # warm up
     conv2d_time = timeit.timeit(conv2d, number=num_iterations)
-    conv2d_depthwise_time = timeit.timeit(conv2dDepthwise, number=num_iterations)
+
+    conv2dSeparable()  # warm up
+    conv2d_separable_time = timeit.timeit(conv2dSeparable, number=num_iterations)
 
     conv2d_avg = conv2d_time / num_iterations
-    conv2d_depthwise_avg = conv2d_depthwise_time / num_iterations
+    conv2d_separable_avg = conv2d_separable_time / num_iterations
 
     print(f"Conv2D Time: {conv2d_avg} seconds per iteration")
-    print(f"Conv2D Depthwise Time: {conv2d_depthwise_avg} seconds per iteration\n")
+    print(f"Conv2D Separable Time: {conv2d_separable_avg} seconds per iteration\n")
 
-    return conv2d_avg, conv2d_depthwise_avg
+    return conv2d_avg, conv2d_separable_avg
 
 
 def main():
+    kernel_sizes = [1, 2, 3]
     batch_sizes = []
     for i in range(65):
         batch_sizes.append(i)
 
-    results_conv2d = []
-    results_conv2d_depthwise = []
+    results_conv2d_kernels = []
+    results_conv2d_separable_kernels = []
+    for kern_size in kernel_sizes:
+        results_conv2d = []
+        results_conv2d_separable = []
+        for batch_size in batch_sizes:
+            conv2d_avg, conv2d_separable_avg = \
+                  benchmark(tf.random.normal((batch_size, 32, 32, 3)), kern_size)
 
-    for batch_size in batch_sizes:
-        conv2d_avg, conv2d_depthwise_avg = benchmark(tf.random.normal((batch_size, 32, 32, 3)))
-        results_conv2d.append(conv2d_avg * 1000)  # convert to ms
-        results_conv2d_depthwise.append(conv2d_depthwise_avg * 1000)  # convert to ms
+            results_conv2d.append(conv2d_avg * 1000)  # convert to ms
+            results_conv2d_separable.append(conv2d_separable_avg * 1000)  # convert to ms
+
+        results_conv2d_kernels.append(results_conv2d)
+        results_conv2d_separable_kernels.append(results_conv2d_separable)
 
     with open("results/depthwise/conv2d.csv", "w") as f:
-        f.write("Batch Size, Conv2d, Conv2d Depthwise\n")
-        for i, batch_size in enumerate(batch_sizes):
-            f.write(f"{batch_size}, {results_conv2d[i]}, {results_conv2d_depthwise[i]}\n")
+        column_names = "Batch Size"
+        for i in kernel_sizes:
+            column_names += f",Conv2D {i}x{i}"
+            column_names += f",Separable Conv2D {i}x{i}"
+        f.write(column_names + "\n")
+
+        for j, batch_size in enumerate(batch_sizes):
+            line = f"{batch_size}"
+            for i, _ in enumerate(kernel_sizes):
+                line += f",{results_conv2d_kernels[i][j]},{results_conv2d_separable_kernels[i][j]}"
+            print(line)
+            f.write(line + "\n")
 
 
 if __name__ == "__main__":
